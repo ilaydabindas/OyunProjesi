54573d9f002ddc1857790ec1a1a4b63480e09a54
ilaydabindas <ilaydabindas@gmail.com>
2026-05-04 19:21:18 +0300

    Yan panel arayüzü ve can sistemi eklendi.
    Yan panel arayüzü oluşturuldu ve içerisine oyun içi bilgiler yazıldı.
    Can sistemi eklendi.
    Kalp sprite'ı eklendi.
    Önceki font tipi değiştirilerek yenisine geçildi.

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "Arkanoid"); // "Arkanoid" başlıklı pencere oluşturur. 
+    sf::RenderWindow window(sf::VideoMode({1000, 600}), "Arkanoid"); // "Arkanoid" başlıklı pencere oluşturur. 
 
     Paddle paddle(340.f,550.f); // paddle oluşturur.
 
@@ -22,16 +22,65 @@ int main()
 
     sf::Font font;
     
-    if (!font.openFromFile("assets/8-bit-hud.ttf")) 
+    if (!font.openFromFile("assets/BoldPixels.ttf")) 
     {
         std::cout << "Font dosyasi bulunamadi!" << std::endl;
     }
+    sf::RectangleShape panel({200.f,600.f});
+    panel.setPosition({800.f,0.f}); 
+    panel.setFillColor(sf::Color(25,25,30));
+
+    sf::RectangleShape ayiriciCizgi({5.f,600.f});
+    ayiriciCizgi.setPosition({800.f,0.f});
+    ayiriciCizgi.setFillColor(sf::Color(100,100,100));
+
     sf::Text scoreText(font);
-    scoreText.setFont(font); // yazı tipi
-    scoreText.setCharacterSize(20); // yazı boyutu
+    scoreText.setCharacterSize(28); // yazı boyutu
     scoreText.setFillColor(sf::Color::White); // Yazı rengi
-    scoreText.setPosition({10.f, 10.f}); // ekranın sol üst köşesi
-    scoreText.setString("Skor: 0"); // başlangıç yazısı
+    scoreText.setPosition({815.f, 30.f}); // sağ panel
+    scoreText.setString("SKOR: 0"); // başlangıç yazısı
+
+    sf::Text canSayisi(font);
+    canSayisi.setCharacterSize(28);
+    canSayisi.setFillColor(sf::Color(180,0,0));
+    canSayisi.setPosition({815.f,100.f});
+    canSayisi.setString("Can:");
+
+    sf::Texture kalp;
+    if(!kalp.loadFromFile("assets/heart.png"))
+    {
+        std::cout << "Kalp resmi bulunamadı!" << std::endl;
+        return -1;
+    }
+    sf::Sprite kalpSprite(kalp);
+    kalpSprite.setScale({0.07f,0.07f}); // kalp.png büyüklüğü 1024x1024 olduğundan küçültüyoruz. 
+    int aktifCanSayisi = 3;
+
+    sf::Text powerup(font);
+    powerup.setCharacterSize(20);
+    powerup.setFillColor(sf::Color(178,102,255));
+    powerup.setPosition({815.f,180.f});
+    powerup.setString(U"Aktif Güçlendirme:"); // başa U koyarak türkçe karakter desteği verdik. 
+    powerup.setStyle(sf::Text::Underlined); // altı çizili.
+
+    sf::Text powerupList(font);
+    powerupList.setCharacterSize(20);
+    powerupList.setFillColor(sf::Color::White);
+    powerupList.setPosition({815.f,205.f});
+    powerupList.setString("- YOK -");
+
+
+    sf::Text ballSpeed(font);
+    ballSpeed.setCharacterSize(22);
+    ballSpeed.setFillColor(sf::Color(224,224,224));
+    ballSpeed.setPosition({815.f,450.f});
+    ballSpeed.setString(U"Top Hızı : %100");
+
+    sf::Text level(font);
+    level.setCharacterSize(24);
+    level.setFillColor(sf::Color(255,200,0));
+    level.setPosition({815.f,520.f});
+    level.setString("LEVEL : 1");
 
 
     while (window.isOpen())
@@ -93,7 +142,7 @@ int main()
                         if (kazanilanPuan > 0)
                         {
                             score += kazanilanPuan; // skoru güncelle. 
-                            scoreText.setString("Skor: "+ std::to_string(score)); // ekrandaki yazıyı güncelle. 
+                            scoreText.setString("SKOR: "+ std::to_string(score)); // ekrandaki yazıyı güncelle. 
                         }
                         ball.reverseY(); // topu ters yöne sektir. 
                         break; // aynı anda iki bloğa birden çarpıp mantık hatası yaratmasını engellemek için döngüyü bitir. 
@@ -103,7 +152,18 @@ int main()
             // topun aşağı düşme kontrolü. 
             if (ball.getPosition().y > 600.f)
             {
-                ball.reset(); // topu tekrar paddle'a bağlar. 
+                aktifCanSayisi--; // bir can azalt. 
+
+                if(aktifCanSayisi >0)
+                {
+                    ball.reset(); // topu tekrar paddle'a bağlar. 
+                }
+                else
+                {
+                    std::cout << "OYUN BİTTİ" << std::endl;
+                    window.close(); // ileride burası game over arayüzüne bağlanacak şu anlık pencereyi kapatıyoruz. 
+                }
+                
             }
         }
 
@@ -118,7 +178,22 @@ int main()
 
         paddle.draw(window); // paddle ekrana çizilir. 
         ball.draw(window); // top ekrana çizilir. 
-        window.draw(scoreText); // skor ekrana çizilir. 
+        window.draw(panel); // paneli çizer.  
+        window.draw(ayiriciCizgi); // ayırıcı çizgiyi çizer.
+        window.draw(scoreText); // aktif skor ekrana çizilir. 
+
+        window.draw(canSayisi); // aktif can sayısını çizer. 
+        for (int i=0; i< aktifCanSayisi; i++)
+        {
+            // her kalbi x ekseninde 30 piksel sağa kaydırarak yerleştir.
+            kalpSprite.setPosition({850.f + (i*32.f),88.f});
+            window.draw(kalpSprite);
+        }
+
+        window.draw(powerup); // aktif güçlendirme başlığını çizer.
+        window.draw(powerupList); // aktif güçlendirmeleri listeler. 
+        window.draw(ballSpeed); // aktif top hızını yazar. 
+        window.draw(level); // aktif leveli çizer. 
 
         window.display(); // ekrana çizilenleri gösterir. 
     }
